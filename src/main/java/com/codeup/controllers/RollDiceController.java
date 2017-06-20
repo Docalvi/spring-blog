a2019f73e76860a454cc6a629a9ec05bf32b7d75
emilio alvarado <emiliolvrd@gmail.com>
2017-06-20 13:37:53 -0500

    roll-dice exercise incomplete

--- src/main/java/com/codeup/controllers/RollDiceController.java
+++ src/main/java/com/codeup/controllers/RollDiceController.java
@@ -1,15 +1,16 @@
 package com.codeup.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
-/**
- * Created by emilioalvarado on 6/19/17.
- */
+
 @Controller
 public class RollDiceController {
-    @GetMapping("/roll-dice/home")
-    public String diceRoll() {
+    @GetMapping("/roll-dice/{n}")
+    public String diceRoll(@PathVariable String n, Model model) {
+        model.addAttribute("n", n);
         return "/profile/roll-dice";
     }
 }
