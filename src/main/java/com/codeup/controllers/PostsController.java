a26ac8a6b63d011359e18fa179de09d347771388
emilio alvarado <emiliolvrd@gmail.com>
2017-06-21 10:26:51 -0500

    Refactored PostsController to utilize the form template to create new posts

--- src/main/java/com/codeup/controllers/PostsController.java
+++ src/main/java/com/codeup/controllers/PostsController.java
@@ -5,10 +5,7 @@ import com.codeup.svcs.PostSvc;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -48,7 +45,13 @@ public class PostsController {
     }
 
     @PostMapping("/post/create")
-    public String savePost() {
+    public String savePost(
+            @RequestParam(name = "title") String title,
+            @RequestParam(name = "body") String body,
+            Model model
+    ) {
+        Post post = new Post(title, body);
+        model.addAttribute("post", post);
 
         return "/posts/create";
     }
